72cb7b7e09c6ce2c4028f9b742251960ac7a135b
Oren Saldanha <orensaldanha@gmail.com>
2023-04-01 13:36:24 +0530

    updated temp

--- Iot/tempDHT11/tempModifiedFile.py
+++ Iot/tempDHT11/tempModifiedFile.py
@@ -1,8 +1,25 @@
-import sys
 import Adafruit_DHT
+import paho.mqtt.client as paho
+import datetime
 import time
+import json
+import pytz
+
+IST = pytz.timezone('Asia/Kolkata')
+
+client = paho.Client()
+client.connect('broker.orensaldanha.live', 1883)
+client.loop_start()
 
 while True:
 	humidity,temperature = Adafruit_DHT.read_retry(11,4)
-	print ("Temp: {0:0.1f} C Humidity: {1:0.1f} %".format(temperature,humidity))
+
+	data = {
+		"temperature": temperature,
+		"humidty": humidity,
+		"timestamp": str(datetime.datetime.now(IST))
+	}
+
+	print(data)
+	(rc, mid) = client.publish('/temp', json.dumps(data), qos=1)
 	time.sleep(1)
