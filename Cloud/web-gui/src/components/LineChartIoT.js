76cd08e8f0cc69cd2b74542484f1dd23055d3a08
Stormshadows <maxmario2001@gmail.com>
2023-04-05 02:50:11 +0530

    Fixed chart congestion

--- Cloud/web-gui/src/components/LineChartIoT.js
+++ Cloud/web-gui/src/components/LineChartIoT.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import moment from "moment";
 import {
   Line,
   LineChart,
@@ -6,27 +6,34 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  Legend
-} from 'recharts';
+  Legend,
+} from "recharts";
 
-const LineChartIoT = ({ data, yaxis}) => {  
-    
-    return (
-        <LineChart width={500} height={300} data={data} margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}>
-          <XAxis
-          dataKey="timestamp"  tickFormatter = {(timestamp) => moment(timestamp).format('HH:mm')} />
-          <YAxis />
-          <Tooltip />
-          <Legend/>
-          <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
-          <Line  stroke="#82ca9d" dataKey={yaxis}   />
-        </LineChart>
-    );
-}
+const LineChartIoT = ({ data, yaxis }) => {
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <XAxis
+        dataKey="timestamp"
+        minTickGap={50}
+        tickFormatter={(timestamp) => moment(timestamp).format("HH:mm")}
+      />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+      <Line stroke="#82ca9d" dataKey={yaxis} />
+    </LineChart>
+  );
+};
 
-export default LineChartIoT
+export default LineChartIoT;
