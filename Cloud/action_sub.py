860d327a8667e44e1c18a09992b023a878b580d0
Oren Saldanha <orensaldanha@gmail.com>
2023-04-04 21:44:28 +0530

    added rpi action code

--- Cloud/action_sub.py
+++ Cloud/action_sub.py
@@ -1,35 +1,14 @@
-from gpiozero import Buzzer
-from time import sleep
 import paho.mqtt.client as mqtt
-
-buzzer = Buzzer(22)
-
-# while True:
-# 	buzzer.on()
-# 	sleep(1)
-# 	buzzer.off()
-# 	sleep(1)
-
-toggle = False
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     client.subscribe("/buzzer")
 
 def on_message(client, userdata, msg):
-	print("Toggle Buzzer")
-	if toggle:
-		buzzer.off()
-		toggle = False
-	else:
-		buzzer.on()
-		toggle = True
+    print("Toggle Buzzer")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect('broker.orensaldanha.live', 1883)
 client.loop_forever()
-
-
-
