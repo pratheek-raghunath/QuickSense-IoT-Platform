2a7235e345a91c27539e172c1dd569c2bf0bbd5e
Stormshadows <maxmario2001@gmail.com>
2023-04-05 14:04:10 +0530

    fixed alerts

--- Cloud/web-gui/src/components/Alert.js
+++ Cloud/web-gui/src/components/Alert.js
@@ -26,18 +26,29 @@ const Alert = () => {
 
   return (
     <div class="bg-red m-20  flex flex-col items-center rounded-lg border border-gray-200  p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <h1
-        id="alert"
-        class="mb-2 text-2xl font-bold tracking-tight "
-        // text-gray-900 dark:text-white
-      >
-        Alerts in temperature real time
+      <h1 id="alert" class="mb-2 text-2xl font-bold tracking-tight ">
+        Alerts in real time
       </h1>
       <ul class="font-normal text-gray-700 dark:text-gray-400">
         {latestalerts.map((alert) => {
           return (
-            <li key={alert.timestamp}>
-              {alert.sensor} - {alert.message} - {alert.timestamp}
+            <li class="w-[90vw] p-6" key={alert.timestamp}>
+              <div
+                id="container"
+                class="rounded-t  border border-black bg-white px-4 py-2 font-bold text-white"
+              >
+                {alert.sensor}
+              </div>
+              <div
+                id="container"
+                class=" flex flex-col rounded-b border border-t-0 border-black  px-4 py-3 text-black"
+              >
+                <div className="text-start">{alert.message}</div>
+
+                <div className="items-end justify-end text-start">
+                  {alert.timestamp.substr(11, 10)}
+                </div>
+              </div>
             </li>
           );
         })}
@@ -45,6 +56,8 @@ const Alert = () => {
     </div>
   );
 };
+{
+}
 
 export default Alert;
 
