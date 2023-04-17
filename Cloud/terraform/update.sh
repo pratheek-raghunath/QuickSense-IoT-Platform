992be7f82e6003d60dcc52df5f95091632cad552
Oren Saldanha <orensaldanha@gmail.com>
2023-04-17 21:33:35 +0530

    fixed container update issue

--- Cloud/terraform/update.sh
+++ Cloud/terraform/update.sh
@@ -1,4 +1,7 @@
 cd /Cloud-based-Paas-Iot-Management-Deployment/Cloud
 sudo docker-compose -f docker-compose.prod.yaml down
 sudo git pull
-sudo docker-compose -f docker-compose.prod.yaml up -d --force-recreate
+sudo cp package.json worker.js schema.js worker
+sudo cp package.json wss.js schema.js wss
+sudo cp package.json api.js schema.js api
+sudo docker-compose -f docker-compose.prod.yaml up -d
