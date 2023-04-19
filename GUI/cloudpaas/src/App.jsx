3c5ad6c299735859bc902c9dbd39095cce2ca31f
Stormshadows <maxmario2001@gmail.com>
2023-04-19 15:43:12 +0530

    test

--- GUI/cloudpaas/src/App.jsx
+++ GUI/cloudpaas/src/App.jsx
@@ -3,18 +3,26 @@ import "./App.css";
 import NavLanding from "./NavLanding";
 import Footer from "./Footer";
 import OverView from "./components/OverView";
+import Analytics from "./components/Analytics";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex min-h-screen flex-col">
-      {/* <NavLanding /> */}
-      {/* <Home /> */}
-      <OverView />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="flex min-h-screen flex-col">
+        <Routes>
+          {/* <NavLanding /> */}
+          {/* <Home /> */}
+          <Route path="/" element={<NavLanding />} />
+          <Route path="/overview" element={<OverView />} />
+          <Route path="/overview/analytics" element={<Analytics />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
