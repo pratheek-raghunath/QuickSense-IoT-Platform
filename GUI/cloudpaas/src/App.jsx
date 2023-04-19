d50efb3f123ac3283ba7c8bc3c48737445afa017
Pratheek Raghunath <r.pratheek01@gmail.com>
2023-04-19 15:24:50 +0530

    Added Routes

--- GUI/cloudpaas/src/App.jsx
+++ GUI/cloudpaas/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
+import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import NavLanding from "./NavLanding";
 import Footer from "./Footer";
 import OverView from "./components/OverView";
-// import Home from "./components/Home";
+import Home from "./components/Home";
+import NavUser from "./NavUser";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="flex min-h-screen flex-col">
-      {/* <NavLanding /> */}
-      {/* <Home /> */}
-      <OverView />
+      <NavLanding />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/overview" element={<OverView />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
--- GUI/cloudpaas/src/components/Home.jsx
+++ GUI/cloudpaas/src/components/Home.jsx
@@ -1,43 +1,21 @@
-// import React    from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import Video from '../assets/vid1.mp4';
+function Home() {
+    return (
+        <div className='hero'>
+        <video autoPlay loop muted id='video' className="w-full h-full object-cover -z-10">
+            <source src={Video} type='video/mp4' />
+        </video>
+        <div className="overlay"></div>
+        <div className="absolute top-0 h-full flex justify-center flex-col text-center w-full m-auto p-4 text-white">
+            <h1>KULLA GANG</h1>
+            <h2>Open Source IoT Platform</h2>
+            <h5>Device management, data collection, processing and visualization for your IoT solution</h5>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-96 rounded"> Learn More</button>
+        </div>
+    </div>
 
-// import {SectionsContainer, Section, Header, Footer} from '../index';
+    );
+}
 
-// const app = document.querySelector('#app');
-
-// const Home = React.createClass({
-//   render() {
-//     let options = {
-//       sectionClassName:     'section',
-//       anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
-//       scrollBar:            false,
-//       navigation:           true,
-//       verticalAlign:        false,
-//       sectionPaddingTop:    '50px',
-//       sectionPaddingBottom: '50px',
-//       arrowNavigation:      true
-//     };
-
-//     return (
-//       <div>
-//         <Header>
-//           <a href="#sectionOne">Section One</a>
-//           <a href="#sectionTwo">Section Two</a>
-//           <a href="#sectionThree">Section Three</a>
-//         </Header>
-//         <Footer>
-//           <a href="">Dcoumentation</a>
-//           <a href="">Example Source</a>
-//           <a href="">About</a>
-//         </Footer>
-//         <SectionsContainer className="container" {...options}>
-//           <Section className="custom-section" verticalAlign="true" color="#69D2E7">Page 1</Section>
-//           <Section color="#A7DBD8">Page 2</Section>
-//           <Section color="#E0E4CC">Page 3</Section>
-//         </SectionsContainer>
-//       </div>
-//     );
-//   }
-// });
-
-// ReactDOM.render(<Home/>, app);
+export default Home;
