7f364bd96bee2fce584a5aad54092ebf870a9248
Oren Saldanha <orensaldanha@gmail.com>
2023-04-20 10:15:47 +0530

    added rpi stats

---
+++ Cloud/scripts/temperature_oren.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as paho
+import psutil
+import datetime
+import time
+import json
+import pytz
+import os
+
+IST = pytz.timezone('Asia/Kolkata')
+
+BROKER_URL = os.environ.get('BROKER_URL')
+USER_ID = os.environ.get('USER_ID')
+
+client = paho.Client()
+client.connect(BROKER_URL, 1883)
+client.loop_start()
+
+while True:
+    data = {
+        "sensor": "temperature",
+        "user_id": USER_ID,
+        "data": {
+            "temperature": psutil.sensors_temperatures()['thinkpad'][0].current,
+            "humidity": psutil.sensors_temperatures()['thinkpad'][0].current
+        },
+        "timestamp": str(datetime.datetime.now(IST))
+    }
+    print(data)
+    (rc, mid) = client.publish(f'/{USER_ID}/data_stream/temperature', json.dumps(data), qos=1)
+    time.sleep(3)
---
+++ Iot/sensors/rpi_stats.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as paho
+import psutil
+import datetime
+import time
+import json
+import pytz
+import os
+
+IST = pytz.timezone('Asia/Kolkata')
+
+BROKER_URL = os.environ.get('BROKER_URL')
+USER_ID = os.environ.get('USER_ID')
+
+client = paho.Client()
+client.connect(BROKER_URL, 1883)
+client.loop_start()
+
+while True:
+    data = {
+        "sensor": "rpi",
+        "user_id": USER_ID,
+        "data": {
+            "cpu_temperature": psutil.sensors_temperatures()['cpu_thermal'][0].current,
+            "memory_usage": psutil.virtual_memory()[2],
+            "cpu_usage": psutil.cpu_percent()
+        },
+        "timestamp": str(datetime.datetime.now(IST))
+    }
+    print(data)
+    (rc, mid) = client.publish(f'/{USER_ID}/data_stream/rpi', json.dumps(data), qos=1)
+    time.sleep(3)
