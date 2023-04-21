6269c9764e439fccb2ef7f02a5e590859999ed65
Stormshadows <maxmario2001@gmail.com>
2023-04-21 19:18:58 +0530

    position fixed

--- GUI/cloudpaas/src/components/Microprocessor.jsx
+++ GUI/cloudpaas/src/components/Microprocessor.jsx
@@ -1,10 +1,11 @@
-const Microprocessor = () => {
-  //navigate to dashboard automatically if auth token is present in local storage
+import "../styles/Microprocessorstyles.css";
 
+const Microprocessor = () => {
   return (
     <div>
       <h1 class="mt-9 flex flex-col items-center text-3xl font-bold tracking-tight text-blue-700">
         Microprocessor
+        <img src="../../src/assets/Rpi.png" className="Rpi-logo" alt="Rpi" />
       </h1>
     </div>
   );
