4dc60f5d971dd414de6dcb57727dad7bf5df9de1
Stormshadows <maxmario2001@gmail.com>
2023-04-25 22:15:55 +0530

    fixed visualisation position

--- GUI/cloudpaas/src/components/Visualization.jsx
+++ GUI/cloudpaas/src/components/Visualization.jsx
@@ -9,19 +9,19 @@ const Visualization = () => {
       <h1 className=" mt-7 flex flex-col items-center text-4xl font-bold text-white">
         Visualization
       </h1>
-      <div className="flow-root">
-        <div class="float-left">
+      <div className="flow-root ">
+        <div class="float-left ml-40">
           <Temperature />
         </div>
-        <div class="float-right">
+        <div class="float-right mr-52">
           <Ultrasonic />
         </div>
       </div>
       <div className="flow-root">
-        <div class="float-left">
+        <div class="float-left ml-40">
           <Accelerometer />
         </div>
-        <div class="float-right">
+        <div class="float-right mr-52">
           <Gas />
         </div>
       </div>
