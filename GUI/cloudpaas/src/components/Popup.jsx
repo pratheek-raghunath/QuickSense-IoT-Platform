8d84cc4c0850e67cfde79c11d571928cde619fe3
Stormshadows <maxmario2001@gmail.com>
2023-04-26 21:03:22 +0530

    sensor page complete

--- GUI/cloudpaas/src/components/Popup.jsx
+++ GUI/cloudpaas/src/components/Popup.jsx
@@ -7,9 +7,35 @@ const Popup = () => {
 
   return (
     <>
-      <button onClick={() => setShowPopup(true)} className=" bg-white">
+      <button
+        // id="model-btn"
+        onClick={() => setShowPopup(true)}
+        className=" float-right mr-5 mt-16 rounded bg-blue-700 px-2 py-2 font-bold text-white hover:bg-blue-700"
+      >
         Add Sensors
       </button>
+      <div className="   items-center rounded-lg    p-40 ">
+        <div class="m-6 flex justify-between rounded-lg bg-white px-6 py-6 font-bold text-black">
+          <div>Pressure</div>
+          <div className=" text-right">Running</div>
+        </div>
+        <div class="m-6 flex justify-between rounded-lg bg-white px-6 py-6 font-bold text-black">
+          <div>Gas</div>
+          <div className=" text-right">Running</div>
+        </div>
+        <div class="m-6 flex justify-between rounded-lg bg-white px-6 py-6 font-bold text-black">
+          <div>Temperature</div>
+          <div className=" text-right">Running</div>
+        </div>
+        <div class="m-6 flex justify-between rounded-lg bg-white px-6 py-6 font-bold text-black">
+          <div>IR</div>
+          <div className=" text-right">Running</div>
+        </div>
+        <div class="m-6 flex justify-between rounded-lg bg-white px-6 py-6 font-bold text-black">
+          <div>Accelerometer</div>
+          <div className=" text-right">Running</div>
+        </div>
+      </div>
       {showPopup && <MyPopup closePopup={closePopup} />}
     </>
   );
--- GUI/cloudpaas/src/components/ShowPopup.jsx
+++ GUI/cloudpaas/src/components/ShowPopup.jsx
@@ -1,14 +1,62 @@
 import "../styles/Popupstyles.css";
+import { useEffect } from "react";
 const MyPopup = ({ closePopup }) => {
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {
+      document.body.style.overflowY = "scroll";
+    };
+  }, []);
+
   return (
     <>
-      <div className="modal-wrapper"></div>
+      <div className="modal-wrapper" onClick={closePopup}></div>
       <div className="modal-container">
-        <h2 className="font-semibold text-white">Sensors</h2>
-        <p className="font-semibold text-white">List of sensors</p>
-        <button className="bg-white" onClick={closePopup}>
-          Save
-        </button>
+        <div className="   items-center rounded-lg    p-2 ">
+          <div class=" m-6 flex  rounded-lg bg-white px-6 py-6 font-bold text-black ">
+            <div className="flex flex-col items-center justify-center">
+              Accelerometer
+            </div>
+            {/* <a onClick={}> */}
+            <img id="plus" src="../../src/assets/plus.png" alt="plus" />
+            {/* </a> */}
+          </div>
+
+          <div class=" m-6 flex  rounded-lg bg-white px-6 py-6 font-bold text-black ">
+            <div className="flex flex-col items-center justify-center">Gas</div>
+            {/* <a onClick={}> */}
+            <img id="plus" src="../../src/assets/plus.png" alt="plus" />
+            {/* </a> */}
+          </div>
+          <div class=" m-6 flex  rounded-lg bg-white px-6 py-6 font-bold text-black ">
+            <div className="flex flex-col items-center justify-center">
+              Pressure
+            </div>
+            {/* <a onClick={}> */}
+            <img
+              id="plus"
+              //   className="mb-4 ml-60 h-4 w-4"
+              src="../../src/assets/plus.png"
+              alt="plus"
+            />
+            {/* </a> */}
+          </div>
+          <div class=" m-6 flex  rounded-lg bg-white px-6 py-6 font-bold text-black ">
+            <div className="flex flex-col items-center justify-center">
+              Temperature
+            </div>
+            {/* <a onClick={}> */}
+            <img id="plus" src="../../src/assets/plus.png" alt="plus" />
+            {/* </a> */}
+          </div>
+
+          {/* <button
+            className="rounded bg-blue-700 px-2 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={closePopup}
+          >
+            Save
+          </button> */}
+        </div>
       </div>
     </>
   );
